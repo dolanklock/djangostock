c835c25859ca54c6b496faa3283741b2ff0b1a9c
Dolan Klock <Dolank16@gmail.com>
2023-08-22 20:56:29 -0400

    major updates

--- quotes/views.py
+++ quotes/views.py
@@ -78,16 +78,39 @@ def home(request):  # request coming from urls and calls this function and passe
         return render(request, 'home.html', {"no_symbol": "Enter a Ticker Symbol..."})
 
 def add_stock(request):
+    # TODO: get querys to show in table on addstock html
     if request.method == "POST":
-        form = forms.StockForm(request.POST or None)
+        form = forms.StockForm(request.POST or None)  # sets 'form' equal to what was typed in to input on webpage
         if form.is_valid():  # checks if valid
             form.save()  # saves to database
             messages.success(request,
                             ('stock has been added successfully'))  # this message gets listed on the admin page inside the new database
             return redirect('addstock')  # this should be the html 'addstock' - redirects it to that html
-    # else:  #TODO: fix this, want code in here to always show. 
+        
     ticker = models.Stock.objects.all()  # this gets the objects of class Stock from models
-    return render(request, "addstock.html", {'ticker': ticker})
+    ticker_sym = [str(t) for t in ticker]  # getting symbols only from objects database for batch api request
+    tackers_format_request = ",".join(ticker_sym)  # getting symbols in one text word for api request
+    request_successful = True
+    try:
+        response = requests.get(f"https://api.iex.cloud/v1/data/CORE/QUOTE/{tackers_format_request}?token={API_KEY}")  # calling batch api request for all ticker symbols in database
+        json_data_batch = response.json()
+    except:
+        request_successful = False
+        return render(request, 'addstock.html', {"error": "No stocks added..."})
+    else:
+        # TODO: can remove this portion below if i figure out how to add ticker id in html while iterating dicitonary json for batch
+        batch_query = []
+        for d, ticker_id in zip(json_data_batch, ticker):  # iterating through ticker here so i can pass ticker id to the url in addstock html for the remove button hyperlink
+            company_data = {}
+            company_data['companyName'] = d['companyName']
+            company_data['iexOpen'] = d['iexOpen']
+            company_data['iexClose'] = d['iexClose']
+            company_data['week52High'] = d['week52High']
+            company_data['week52Low'] = d['week52Low']
+            company_data['marketCap'] = d['marketCap']
+            company_data['ticker_id'] = ticker_id.id
+            batch_query.append(company_data)
+        return render(request, "addstock.html", {'ticker': ticker, "batch": batch_query, "request_successful": request_successful})
 
 def delete_stock(request, stock_id):
     item = models.Stock.objects.get(pk=stock_id)  # pk is primary key. getting all objects in database 'stock'. and then getting the specific item that matches id passed in
@@ -97,3 +120,4 @@ def delete_stock(request, stock_id):
 
 def about(request):
     return render(request, 'about.html', {})
+
